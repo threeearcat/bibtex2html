1c386b2a73bb400675f0d4b93cc3aed40c71995b
Dae R. Jeong <dae.r.jeong@snu.ac.kr>
2026-06-19 09:35:15 +0900

    Preserve slash in conference names

--- bibtex2html.py
+++ bibtex2html.py
@@ -229,7 +229,7 @@ def extract_crossref(bibfile):
         if resolved is not None:
             title = resolved
         title = title.replace(r"\&", "&")
-        title = re.sub(r"[^a-zA-Z\s\n\.0-9\(\)&]", " ", title)
+        title = re.sub(r"[^a-zA-Z\s\n\.0-9\(\)&/]", " ", title)
         title = re.sub(r"\s+", " ", title)
         return title.strip()
 
